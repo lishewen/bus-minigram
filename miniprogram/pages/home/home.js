e6e437c6c09b73cec48a679f88b4318e7d671424
算神 <lishewen@msn.com>
2018-02-13 17:27:42 +0800

    搜索UI整合到首页

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -4,7 +4,150 @@ const app = getApp()
 
 Page({
   data: {
+    focused: false
+  },
+  searchFocus: function () {
+    this.setData({
+      focused: true
+    })
+    this.searchInit();
+  },
+  tapCancel: function () {
+    this.routes = null;
+    this.stops = null;
+    this.setData({
+      focused: false,
+      history: null,
+      searchText: '',
+      searchStops: null,
+      searchRoutes: null
+    })
+  },
+  searchConfirm: function (event) {
+    var searchText = event.detail.value;
+    this.setData({
+      searchText: searchText
+    })
+    this.search(searchText)
+  },
+  searchInput: function (event) {
+    var searchText = event.detail.value;
+    this.setData({
+      searchText: searchText
+    })
+    this.search(searchText)
+  },
+  searchClear: function (event) {
+    this.setData({
+      focused: true,
+      searchText: '',
+      searchStops: null,
+      searchRoutes: null
+    })
+    this.searchInit();
+  },
+  searchInit: function () {
+    var history = util.loadHistory();
+    this.setData({
+      history: history ? history : []
+    })
+  },
+  search: function (word) {
+    var self = this;
+    let foldCount = 4;
+    wx.request({
+      url: "https://jbwx.lishewen.com/api/bus/findRouteByName?routeName=" + encodeURIComponent(word),
+      success: function (res) {
+        if (res.data.result != 0) {
+          wx.showToast({
+            image: "/resources/error-empty.png",
+            title: res.data.message
+          })
+          return;
+        }
+        var items = res.data.items;
+        var routes = [];
+        for (var i in items) {
+          var item = items[i];
+          routes.push(item.routes[0]);
+        }
+        self.routes = routes;
+        self.setData({
+          searchRoutes: routes.slice(0, foldCount),
+          routeFold: routes.length > foldCount
+        })
+      },
+      fail: function () {
+        wx.showToast({
+          image: "/resources/error-network.png",
+          title: '请求失败请重试',
+        })
+      },
+      complete: function () {
+        wx.stopPullDownRefresh()
+      }
+    })
 
+    wx.request({
+      url: "https://jbwx.lishewen.com/api/bus/findStopByName?stopName=" + encodeURIComponent(word),
+      success: function (res) {
+        if (res.data.result != 0) {
+          wx.showToast({
+            image: "/resources/error-empty.png",
+            title: res.data.message
+          })
+          return;
+        }
+        var items = res.data.items;
+        var stops = [];
+        for (var i in items) {
+          var item = items[i];
+          stops.push(item.stops[0]);
+        }
+        self.stops = stops;
+        self.setData({
+          searchStops: stops.slice(0, foldCount),
+          stopFold: stops.length > foldCount
+        })
+      },
+      fail: function () {
+        wx.showToast({
+          image: "/resources/error-network.png",
+          title: '请求失败请重试',
+        })
+      },
+      complete: function () {
+        wx.stopPullDownRefresh()
+      }
+    })
+  },
+  routeMore: function () {
+    this.setData({
+      searchRoutes: this.routes,
+      routeFold: false
+    })
+  },
+  stopMore: function () {
+    this.setData({
+      searchStops: this.stops,
+      stopFold: false
+    })
+  },
+  tapSearchRoute: function (e) {
+    var ds = e.currentTarget.dataset;
+    util.saveHistory({
+      type: "route",
+      routeId: ds.routeid,
+      routeName: ds.routename
+    })
+  },
+  tapSearchStop: function (e) {
+    var ds = e.currentTarget.dataset;
+    util.saveHistory({
+      type: "stop",
+      stopId: ds.stopid,
+      stopName: ds.stopname
+    })
   },
   isLoad: false,
   onLoad: function () {
